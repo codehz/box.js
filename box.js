663e8615c8cb052bddf8f7fd7691595e30904848
CodeHz <CodeHz@Outlook.com>
2017-07-10 21:46:08 +0800

    el add string support

--- box.js
+++ box.js
@@ -337,9 +337,24 @@
             [symbols.element]: target.match(/[a-zA-Z][^\.#\[\]]*/g)[0]
         };
         if (/#[a-zA-Z][^\.#\[\]]*/g.test(target)) addition.id = target.match(/#[a-zA-Z][^\.#]*/g)[0].slice(1);
-        if (/\.[a-zA-Z][^\.#\[\]]*/g.test(target)) addition[symbols.classList] = target.match(/\.[a-zA-Z][^\.#\[\]]*/g).map(x => x.slice(1));
-        if (/\[[a-zA-Z][^.#\[\]]*=[^\]\[]+\]/g.test(target)) target.match(/\[[a-zA-Z][^.#\[\]]*=[^\]\[]+\]/g).forEach(equ => addition[equ.match(/\[([a-zA-Z][^.#\[\]]+)=/)[1]] = JSON.parse(equ.match(/=([^\]\[]+)\]/)[1]));
-        return (obj = {}) => Object.assign(obj, addition);
+        if (/\.[a-zA-Z][^\.#\[\]]*/g.test(target))
+            addition[symbols.classList] = target.match(/\.[a-zA-Z][^\.#\[\]]*/g).map(x => x.slice(1));
+        if (/\[[a-zA-Z][^.#\[\]]*=[^\]\[]+\]/g.test(target))
+            target
+                .match(/\[[a-zA-Z][^.#\[\]]*=[^\]\[]+\]/g)
+                .forEach(
+                    equ =>
+                        (addition[equ.match(/\[([a-zA-Z][^.#\[\]]+)=/)[1]] = JSON.parse(equ.match(/=([^\]\[]+)\]/)[1]))
+                );
+        return (obj = {}) =>
+            Object.assign(
+                typeof obj === `object`
+                    ? obj
+                    : {
+                        [symbols.text]: `${obj}`
+                    },
+                addition
+            );
     }
 
     defineConst(
